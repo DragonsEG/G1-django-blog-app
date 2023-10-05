9824b3e978910a70c74fccdda2be1aeab9c7ccf4
Ahmedreda122 <ahmalolireda12@gmail.com>
2023-10-05 09:33:21 +0300

    Pushing to Main

--- myApp/urls.py
+++ myApp/urls.py
@@ -5,10 +5,10 @@ from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+  path("", views.index, name="index"),
   path("register/", views.register, name="register"),
   path("login/", views.loginView, name="login"),
   path("logout", views.logoutRequest, name= "logout"),
-  path("", views.index, name="index"),
   path("blog/create", views.createBlog, name="createBlog"),
   path("blog/", views.showBlogs, name="showBlogs"),
   path("blog/id/<int:id>/", views.blogPage, name="blogPage"),
@@ -34,9 +34,9 @@ urlpatterns = [
     path('blog/create_category/', views.Category_create, name='category_create'),
     path('blog/edit_category/<int:category_id>/', views.category_edit, name='category_edit'),
     path('blog/delete_category/<int:category_id>/', views.category_delete, name='category_delete'),
-  path('change-password/', PasswordChangeView.as_view(), name='password_change'),
-  path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change_password.html',success_url='/password-change-done/'  ), name='password_change'),
-  path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='blog/password_change_done.html'), name='password_change_done'),
+    path('change-password/', PasswordChangeView.as_view(), name='password_change'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='change_password.html',success_url='/password-change-done/'  ), name='password_change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='blog/password_change_done.html'), name='password_change_done'),
   # --------------------------------------
     path('blog/categories/', category_list, name='category_list'),
     path('blog/category/', category_list1, name='category_list1'),
